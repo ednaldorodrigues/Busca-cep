917efc131bc1c0a6ac2ed0e369cf245cb4fd5215
ednaldorodrigues <ednaldo.rodrigues.067@live.com>
2023-03-08 13:22:51 -0300

    Busca-cep

---
+++ JS/main.js
@@ -0,0 +1,62 @@
+const input = document.querySelector('#input');
+const btn = document.querySelector('#btn');
+const erro = document.querySelector('#erro')
+const cidade = document.querySelector('#resultado-cidade');
+const estado = document.querySelector('#resultado-uf');
+const bairro = document.querySelector('#bairro');
+const rua = document.querySelector('#rua');
+
+function enviar(event) {
+    erro.innerHTML = ''
+    erro.classList.remove('active')
+    estado.innerHTML = ''
+    estado.classList.remove('ativo')
+    cidade.innerHTML = ''
+    cidade.classList.remove('ativo')
+    rua.innerHTML = ''
+    rua.classList.remove('ativo')
+    bairro.innerHTML = ''
+    bairro.classList.remove('ativo')
+    event.preventDefault();
+    const cep = input.value;
+    puxaCep(cep);
+    
+}
+
+btn.addEventListener('click', enviar);
+
+
+function puxaCep(cep) {
+    fetch(`https://viacep.com.br/viacep.com.br/ws/${cep}/json/`)
+    .then(r => r.json())
+    .then(info => {
+
+        if(info.localidade !== '') {
+            cidade.innerText = `CIDADE: ${info.localidade.toUpperCase()}`
+            cidade.classList.add('ativo')
+        }
+        else {cidade.classList.remove('ativo')}
+
+        if(info.uf !== ''){
+            estado.innerText = `UF: ${info.uf.toUpperCase()}`
+            estado.classList.add('ativo')
+        }
+        else {cidade.classList.remove('ativo')}
+
+        if(info.bairro !== '') {
+            bairro.innerText = `BAIRRO: ${info.bairro.toUpperCase()}`
+            bairro.classList.add('ativo')
+        }
+         else {bairro.classList.remove('ativo')}
+
+        if(info.logradouro !== '') {
+            rua.innerText = `LOGRADOURO: ${info.logradouro.toUpperCase()}`
+            rua.classList.add('ativo')
+        }
+        else {rua.classList.remove('ativo')}
+        
+    }).catch(() => {
+        erro.classList.add('active')
+        erro.innerHTML = 'O CEP não foi encontrado ou não existe !'
+    })
+}
